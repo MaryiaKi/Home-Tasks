77cc4cf38d0faeb2706543586f78f8661dc88b2c
Maryia Kirpichonak <mkirpichenok@gmail.com>
2024-08-20 23:01:05 +0300

    Add homework for Lesson 14

---
+++ src/main/java/Lesson_14/Factorial.java
@@ -0,0 +1,20 @@
+package Lesson_14;
+
+public class Factorial {
+    static String NegativeNumberErrorMessage = "Entered number cannot be negative";
+    public static String calculateFactorial(int n) {
+        int curr_fact = 1;
+        if (n < 0) {
+            return NegativeNumberErrorMessage;
+        } else if (n == 0) {
+            return String.valueOf(curr_fact);
+        } else {
+            for (int i = 1; i <= n; i++) {
+                curr_fact = curr_fact * i;
+            }
+            return String.valueOf(curr_fact);
+        }
+    }
+}
+
+
---
+++ src/test/java/TestFactorial.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static Lesson_14.Factorial.calculateFactorial;
+
+public class TestFactorial {
+    @Test
+    @DisplayName("Check that Factorial 7 is equal 5040")
+    public void testFactorial() {
+        Assertions.assertEquals("5040", calculateFactorial(7));
+    }
+    @Test
+    @DisplayName("Check that Factorial 0 is equal 1")
+    public void testFactorial1() {
+        Assertions.assertEquals("1", calculateFactorial(0));
+    }
+    @Test
+    @DisplayName("Check Error Message for Negative Factorial")
+    public void testFactorial2() {
+        Assertions.assertEquals("Entered number cannot be negative", calculateFactorial(-1));
+    }
+}
