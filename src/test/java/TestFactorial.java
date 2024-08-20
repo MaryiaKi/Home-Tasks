d978ca698a69b18e407734c0752c8fab6194b57e
Maryia Kirpichonak <mkirpichenok@gmail.com>
2024-08-20 23:07:05 +0300

    Add 'Factorial of 1 verification' to the homework for Lesson 14

--- src/test/java/TestFactorial.java
+++ src/test/java/TestFactorial.java
@@ -7,17 +7,25 @@ import static Lesson_14.Factorial.calculateFactorial;
 public class TestFactorial {
     @Test
     @DisplayName("Check that Factorial 7 is equal 5040")
-    public void testFactorial() {
+    public void testFactorial1() {
         Assertions.assertEquals("5040", calculateFactorial(7));
     }
+
     @Test
     @DisplayName("Check that Factorial 0 is equal 1")
-    public void testFactorial1() {
+    public void testFactorial2() {
         Assertions.assertEquals("1", calculateFactorial(0));
     }
+
+    @Test
+    @DisplayName("Check that Factorial 1 is equal 1")
+    public void testFactorial3() {
+        Assertions.assertEquals("1", calculateFactorial(1));
+    }
+
     @Test
     @DisplayName("Check Error Message for Negative Factorial")
-    public void testFactorial2() {
+    public void testFactorial4() {
         Assertions.assertEquals("Entered number cannot be negative", calculateFactorial(-1));
     }
 }
